830f73f78ae4c3e10350a5b07c57799d0058a11b
Gilmed0526git push origin maingit push origin main$ git remote -vgit commit -m page <gilbertomedina0526@gmail.com>
2021-07-07 14:58:51 -0400

    created web server, multiple query and single parameter funcions

--- server.js
+++ server.js
@@ -43,12 +43,27 @@ function filterByQuery(query, animalsArray) {
     return filteredResults;
 };
 
+function findById(id, animalsArray) {
+    const result = animalsArray.filter(animal => animal.id === id)[0];
+    return result;
+};
+
 app.get('/api/animals', (req, res) => {
     let result = animals;
     if (req.query) {
         result = filterByQuery(req.query, result);
     }
     res.json(result);
+});
+
+app.get('/api/animals/:id', (req, res) => {
+    const result = findById(req.params.id, animals);
+    if (result){
+        res.json(result);
+    }else{
+        res.send(404);
+    }
+    
 });
 
 //we listen method to app to make our server listen
